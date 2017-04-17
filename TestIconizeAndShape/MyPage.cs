78252a895bcaede6cd0942995df1a245d6ac9e58
Alessandro Caliaro <acaliaro@libero.it>
2017-04-17 21:54:53 +0200

    first commit

---
+++ TestIconizeAndShape/MyPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FormsPlugin.Iconize;
+using Plugin.Iconize;
+using Xamarin.Forms;
+using XFShapeView;
+
+namespace TestIconizeAndShape
+{
+	public class MyPage : ContentPage
+	{
+		public MyPage()
+		{
+
+			Label l = new Label() {Text  = "Press the icon", VerticalOptions = LayoutOptions.Center, FontSize = 40 };
+
+			IconImage iconImage = new IconImage() { IconColor = Color.White, IconSize = 40, VerticalOptions = LayoutOptions.Center};
+			iconImage.Icon = Iconize.FindIconForKey("ion-information").Key;
+
+
+			var circle = new ShapeView
+			{
+				ShapeType = ShapeType.Circle,
+				HeightRequest = 50,
+				WidthRequest = 50,
+				Color = Color.Green,
+				VerticalOptions = LayoutOptions.Center,
+				CornerRadius = 5,
+				BorderColor = Color.Red,
+				BorderWidth = 1f,
+				Content = iconImage
+			};
+
+			StackLayout sl = new StackLayout() { VerticalOptions = LayoutOptions.Center, Orientation = StackOrientation.Horizontal, Children =  { l, circle }, Margin = new Thickness(0,20,0,0) };
+
+			Content = sl;
+
+		}
+	}
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using XFShapeView.iOS;
+
+namespace TestIconizeAndShape.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+
+			global::Xamarin.Forms.Forms.Init();
+
+			Plugin.Iconize.Iconize.With(new Plugin.Iconize.Fonts.IoniconsModule());
+
+			LoadApplication(new App());
+
+			ShapeRenderer.Init();
+			FormsPlugin.Iconize.iOS.IconControls.Init();
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
